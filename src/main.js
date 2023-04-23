449351b6dbc98cbb5443c3484475b5ca9dd42061
AbApin <apinyan2005@mail.ru>
2023-04-23 14:59:16 +0400

    awesome

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import '@fortawesome/fontawesome-free/css/all.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUser)
 
 import './assets/main.css'
 
